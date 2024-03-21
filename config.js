8bde995b3ea0be98b79101d67b959b3b760acc54
hasib <mdhsbaskari@gmail.com>
2024-03-21 17:40:03 +0600

    config.js => es6 -> commonjs

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
-export const MONGODB_CONNECTION = "mongodb://localhost:27017/"
-export const RUNNING_PORT = 5000
+module.exports = {
+    MONGODB_CONNECTION: "mongodb+srv://rnadoha:oAwZCsQ0r25eRlE7@final.pgrmvz5.mongodb.net/",
+    RUNNING_PORT: 5000,
 
-export const JWT_SECRET = "secret"
-export const JWT_EXPIRATION_TIME = "1h"
+    JWT_SECRET: "secret",
+    JWT_EXPIRATION_TIME: "1h",
 
-export const SALT_ROUNDS = 10
+    SALT_ROUNDS: 10,
 
-export const EMAIL_USER = ""
-export const EMAIL_PASSWORD = ""
-export const EMAIL_HOST = "smtp.gmail.com"
-export const EMAIL_PORT = 465
+    EMAIL_USER: "",
+    EMAIL_PASSWORD: "",
+    EMAIL_HOST: "smtp.gmail.com",
+    EMAIL_PORT: 465,
+}
