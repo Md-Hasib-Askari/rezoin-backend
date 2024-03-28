8bd56ecd1ce644be274dbc113e972fdd51d93cd3
Ashraful Asif <ashrafulasif260@gmail.com>
2024-03-28 22:17:12 +0600

    poke updated

--- src/controllers/PokeController.js
+++ src/controllers/PokeController.js
@@ -3,27 +3,40 @@ const UserModel = require("../models/UserModel");
 
 const pokeUser = async (req, res) => {
   try {
-    const { sender_id, recipient_id } = req.body;
+    const { sender_id, recipient_id } = req.body; 
 
     // Check if sender and recipient exist
-    const sender = await UserModel.findById(sender_id);
-    const recipient = await UserModel.findById(recipient_id);
+    const [sender, recipient] = await Promise.all([
+      UserModel.findById(sender_id),
+      UserModel.findById(recipient_id),
+    ]);
     if (!sender || !recipient) {
       return res.status(404).json({ message: "Sender or recipient not found" });
     }
-
-    // Check if the recipient has been poked before
-    let poke = await PokeModel.findOne({ sender_id, recipient_id });
+    // If no poke exists, create a new poke
+    let poke = await PokeModel.findOne({ sender_id, recipient_id }); 
 
     if (!poke) {
-      poke = new PokeModel({ sender_id, recipient_id });
-    } else {
-      poke.isPoked = !poke.isPoked;
+      poke = new PokeModel({ sender_id, recipient_id, isPoked: true });
+      await poke.save();
+      return res.status(201).json({ message: "Poke Successful" });
+    } 
+
+    // If recipient has poked back, toggle isPoked for both sender and recipient
+    const recipientPoke = await PokeModel.findOne({
+      sender_id: recipient_id,
+      recipient_id: sender_id,
+    });
+    if (recipientPoke) {
+      poke.isPoked = true;
+      recipientPoke.isPoked = false;
+      await Promise.all([poke.save(), recipientPoke.save()]);
+      return res.status(201).json({ message: "Poke Successful" });
     }
-
-    await poke.save();
-
-    res.status(201).json({ message: "Poke sent successfully" });
+     // If recipient hasn't poked back, return error
+    return res.status(400).json({
+      message: "Recipient has not poked back, sender cannot poke again",
+    });
   } catch (error) {
     console.error("Error poking user:", error);
     res.status(500).json({ message: "Internal server error" });
